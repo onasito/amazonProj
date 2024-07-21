7b622593b56c2a4f7f5683fd297de3854b179e38
onasito <oarrechavala0@gmail.com>
2024-07-21 11:53:49 -0700

    Updated the total cart count

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -7,10 +7,12 @@ export function renderPaymentSummary()
 {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let cartQuantity = 0;
 
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId)
     productPriceCents += product.priceCents * cartItem.quantity
+    cartQuantity += cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
@@ -27,7 +29,7 @@ export function renderPaymentSummary()
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${cartQuantity}):</div>
       <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
     </div>
 
